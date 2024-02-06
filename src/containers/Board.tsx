1b508ed39977da615bcd410ba3976ea97697867d
Saber Chkiri <75795312+MohamedSaberChkiri@users.noreply.github.com>
2024-02-06 11:37:04 +0100

    added reset button

--- src/containers/Board.tsx
+++ src/containers/Board.tsx
@@ -3,6 +3,7 @@
 import Square from "@/components/Square";
 import { useState } from "react";
 import '../app/globals.css'
+import { Button } from "@/components/ui/button";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
@@ -23,7 +24,15 @@ export default function Board() {
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X")
   };
 
+
+  function reset(){
+    setSquares(Array(9).fill(null))
+    setWinner(null)
+    setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? "X" : "O")
+  }
+
   return (
+    <div className="h-full w-full flex items-center justify-center flex-col gap-6">
     <div className=" w-[400px] h-[400px] grid grid-rows-3 grid-cols-3 gap-2 mx-auto mt-[200px]">
       {Array(9).fill(null).map((_, i) => {
         return (
@@ -35,6 +44,10 @@ export default function Board() {
           />
         );
       })}
+    </div>
+
+    <Button onClick={reset} className="">Reset</Button>
+
     </div>
   );
 }
